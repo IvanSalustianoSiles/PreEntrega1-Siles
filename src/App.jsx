aa42d73648d46056df54e9cf96945d135086b8a1
Ivan Siles <silesivansalustiano@gmail.com>
2024-02-07 18:57:56 -0300

    1.0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
-import ItemListContainer from "./components/pages/ItemListContainer"
 import Navbar from "./components/layout/Navbar"
+import ItemListContainer from "./components/pages/ItemListContainer"
 function App() {
   return (
     <div>
       <Navbar />
-      <ItemListContainer greeting={"Bienvenidos!"} />
+      <ItemListContainer />
     </div>
   )
 }
