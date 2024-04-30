f822ceea8c90d29f9628bba179cc0b645c06bb04
Ivan Siles <silesivansalustiano@gmail.com>
2024-04-29 22:33:45 -0300

    1.0.3

--- products-mock.js
+++ products-mock.js
@@ -1,4 +1,4 @@
-export const products = [
+export const myProducts = [
     { title: "title1", description: "description1", price: "price1", thumbnail: "thumbnai1", code: "code1", stock: "stock1", category: "buzos", status: true },
     { title: "title2", description: "description2", price: "price2", thumbnail: "thumbnai2l", code: "code2", stock: "stock2", category: "sabanas", status: true },
     { title: "title3", description: "description3", price: "price3", thumbnail: "thumbnail3", code: "code3", stock: "stock3", category: "buzos", status: true },
